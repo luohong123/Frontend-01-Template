f3d0226cdf3f8e7ec00579f95790ad098060e271
luohong123 <3300536651@qq.com>
2020-05-27 23:27:33 +0800

    week07

---
+++ week07/code&task/toy-browser/render.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: lh
+ * @Date: 2020-05-27 23:24:20
+ * @LastEditors: lh
+ * @LastEditTime: 2020-05-27 23:25:47
+ * @Description: 
+ * @email: 3300536651@qq.com
+ */ 
+function render(viewport,element) {
+    
+}
