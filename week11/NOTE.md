c9880b80646160f9b4d04f4d3969f2d7a62ea73a
luohong123 <3300536651@qq.com>
2020-06-24 22:56:54 +0800

    week11

--- week11/NOTE.md
+++ week11/NOTE.md
@@ -1,1 +1,51 @@
 # 每周总结可以写在这里
+
+## 课堂笔记
+
+2020-06-18
+
+- winter:大部分情况推荐使用 await，有 await，.then 可以不用
+  运行时的语法 I think await 是 promise 的语法
+### Promise异步编程
+
+### 寻路问题
+
+上下左右走，斜向走，一个起点 一个终点
+【搜索】
+
+当地图比较大时，推荐使用一维数组
+
+```js
+// 旧方法 创建一个 10000 长度 的数组
+let map = Array(10001)
+  .join(0)
+  .split('')
+  .map((s) => Number(s))
+```
+
+localStorage 不靠谱，宁愿存 cookie 也不愿意存 localStorage
+
+### 编辑距离
+
+leetcode 72 题
+
+- 前端哪些地方会用到算法
+  广义的算法
+  狭义的算法(分治、递归、动态规划)
+
+> winter 做前端一定要学会可视化
+> binaryHeap 不要用对象，对象在 js 中占内存特别大，要用数组
+
+### 正则
+
+- exec
+- lastIndex
+
+```js
+let lastIndex = 0;
+let token;
+do{
+    token = inputElement.exec(source);
+    console.log(token);
+}
+```
---
+++ week11/code&task/path/path.js
@@ -0,0 +1,120 @@
+/*
+ * @Author: lh
+ * @Date: 2020-06-18 21:31:04
+ * @LastEditors: lh
+ * @LastEditTime: 2020-06-20 20:20:09
+ * @Description: 
+ * @email: 3300536651@qq.com
+ */
+
+class Sorted {
+    constructor(data, compare) {
+        this.data = data;
+        this.compare = compare;
+    }
+    /**
+     * 拿一个最小的
+     */
+    take() {
+        if (!this.data.length) return;
+        let min = this.data[0];
+        let minIndex = 0;
+        for (let i = 1; i < this.data.length; i++) {
+            if (this.compare(this.data[i], min) > 0) {
+                min = this.data[i];
+                minIndex = i;
+            }
+        }
+        this.data[minIndex] = this.data[this.data.length - 1];
+        this.data.pop();
+        return min;
+    }
+    insert(v) {
+        this.data.push(v);
+    }
+}
+// let a = new Sorted([2,1,4,6,7],function(a,b){return a-b})
+// a.take()
+let map = localStorage.map ? JSON.parse(localStorage.map) : new Array(10000).fill(0);
+let container = document.getElementById('container');
+for (let y = 0; y < 100; y++) {
+    for (let x = 0; x < 100; x++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        if (map[y * 100 + x] === 1) {
+            cell.style.backgroundColor = 'black';
+        }
+        cell.addEventListener('mouseover', () => {
+            if (mouse) {
+                if (clear) {
+                    cell.style.backgroundColor = '';
+                    map[y * 100 + x] = 0;
+                } else {
+                    cell.style.backgroundColor = 'black';
+                    map[y * 100 + x] = 1;
+                }
+            }
+        })
+        container.appendChild(cell);
+    }
+}
+let mouse = false;
+let clear = false;
+// 橡皮擦
+document.addEventListener('mousedown', e => {
+    mouse = true;
+    // 点击鼠标右键时
+    clear = (e.which === 3);
+})
+document.addEventListener('mouseup', () => mouse = false);
+document.addEventListener('contextmenu', e => e.preventDefault());
+
+function sleep(t) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, t);
+    });
+}
+async function findPath(map, start, end) {
+    map = map.slice();
+    let queue = [start];
+
+    container.children[start[1] * 100 + start[0]].style.backgroundColor = 'green';
+    container.children[end[1] * 100 + end[0]].style.backgroundColor = 'red';
+    async function insert([x, y], pre) {
+        if (map[100 * y + x] !== 0)
+            return;
+        // 到达边界
+        if (x < 0 || y < 0 || x >= 0 || x >= 100 || y >= 100) return;
+        // 不走回头路
+        map[100 * y + x] = pre;
+        // container.children[y * 100 + x].style.backgroundColor = 'lightgreen';
+        // await sleep(1);
+        queue.push([x, y]);
+    }
+
+    while (queue.length) {
+        let [x, y] = queue.shift();
+        console.log(x, y);
+        if (x === end[0] && y === end[1]) {
+            let path = [];
+            while (x !== start[0] || y !== start[1]) {
+                path.push([x, y]);
+                container.children[y * 100 + x].style.backgroundColor = 'purple';
+                [x, y] = map[y * 100 + x];
+            }
+            return path;
+        }
+        await insert([x - 1, y], [x, y]);
+        await insert([x + 1, y], [x, y]);
+        await insert([x, y - 1], [x, y]);
+        await insert([x, y + 1], [x, y]);
+
+        await insert([x - 1, y - 1], [x, y]);
+        await insert([x + 1, y - 1], [x, y]);
+        await insert([x - 1, y + 1], [x, y]);
+        await insert([x + 1, y + 1], [x, y]);
+
+    }
+    return null;
+}
+// findPath(map,[0,0],[50,50]);
