01c97f40f165f22d3f3b32a56a151911203a53fd
luohong <luohong@neusoft.com>
2020-08-19 22:46:53 +0800

    test

--- week19/code&task/toy-tool/generators/app/templates/main.test.js
+++ week19/code&task/toy-tool/generators/app/templates/main.test.js
@@ -1,11 +1,14 @@
 /*
  * @Author: lh
  * @Date: 2020-08-19 17:45:17
- * @LastEditors: lh
- * @LastEditTime: 2020-08-19 17:48:00
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-08-19 22:46:34
  * @Description: 
  * @email: 3300536651@qq.com
  */
 import "../src/main.js";
 // 断言
 let assert = require('assert');
+it('8 should equals 8', function () {
+  assert.equal(8, 8);
+});
